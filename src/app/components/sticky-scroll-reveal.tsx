ad9b685c2ee77fe529c4e1472e3e9225285a84ae
marcreniel <marcbernardino2005@gmail.com>
2024-07-22 00:48:34 -0700

    changed things, added footer

--- src/app/components/sticky-scroll-reveal.tsx
+++ src/app/components/sticky-scroll-reveal.tsx
@@ -86,7 +86,7 @@ export const StickyScroll = () => {
         animate={{
             backgroundColor: backgroundColors[activeCard % backgroundColors.length],
         }}
-        className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10 no-scrollbar"
+        className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10 no-scrollbar xl:mt-48"
         ref={ref}
         >
         <div className="div relative flex items-start px-4">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { MacbookScroll } from "./components/macbook-scroll";
 import { StickyScroll } from "./components/sticky-scroll-reveal";
 import Navigation from "./components/navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +9,13 @@ export default function Home() {
       <Navigation />
       <MacbookScroll src="/landing.png" />
       <StickyScroll />
+      <footer className="w-full py-4 bg-harvest-gold-100">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-600 ">
+          made with <span className="text-red-500">❤️</span> in 🌁 by <Link href="http://notmarc.me">marc</Link>
+        </p>
+      </div>
+    </footer>
     </main>
   );
 }
